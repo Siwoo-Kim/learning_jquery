6b3803d11b9a8015b9d8fe0b78d1a287f4eff659
Siwoo-Kim <skim327@myseneca.ca>
2018-03-09 17:39:39 -0500

    Jquery

--- js/chapter-1.js
+++ js/chapter-1.js
@@ -1,6 +1,22 @@
 
 $(document).ready(function () {
-   var $cIntroBtns = $('#c0_intro .btn-success').hide();
+
+
+    function assertTrue(expression,context){
+        if(expression){
+            context.append(
+                $(document.createElement('p'))
+                    .text('Assertion Success')
+                    .css({border : '2px solid blue', color: 'white', backgroundColor : 'blue'}));
+        }else {
+            context.append(
+                $resultEl.append($(document.createElement('p'))
+                    .text('Assertion Fail')
+                    .css({border: '2px solid red',color: 'white', backgroundColor : 'red'})));
+        }
+    }
+
+    var $cIntroBtns = $('#c0_intro .btn-success').hide();
    // $cIntroBtns.hide();
    // $cIntroBtns.text('New Content');
    // $cIntroBtns.addClass('updatedContent');
@@ -63,6 +79,100 @@ $(document).ready(function () {
     var prevText = $paraEl.text();
     //$paraEl[0].innerText = prevText + ' '+$paraEl.find('em').length+' elements! ';
     $paraEl.text( prevText + ' '+$('#c1_16 p:first').find('em').length+' elements! ');
+
+    var $c1_17 = $('#c1_17');
+    var $lastPEl = $c1_17.find('p:last');
+
+    var lengthWithoudEnd = $c1_17.find('p').filter(':not(:last)').filter('.middle').length;
+    var lengthWithEnd = $c1_17.find('p').filter(':not(:last)').filter('.middle').end().length;
+    var lengthFromSpan = $c1_17.find('p').find('.middle').find('span').end().end().end().length;
+    $lastPEl
+        .text( $lastPEl.text() + ' length without end() : '+lengthWithoudEnd );
+    $lastPEl.text( $lastPEl.text() + ' length with end() : '+lengthWithEnd);
+    $lastPEl.text( $lastPEl.text() + ' length from span by end() : '+lengthFromSpan);
+
+    var $c1_19 = $('#c1_19');
+    var $resultEl = $c1_19.find('.result');
+
+    assertTrue( $c1_19.find('p:eq(1)').text() === 'Paragraph 2' ,$resultEl);
+    assertTrue( $c1_19.find('p:eq(1)').prev().text() === 'Paragraph 1' ,$resultEl);
+    assertTrue( $c1_19.find('p:eq(1)').parent()[0] == $c1_19.find('.col-6')[0] ,$resultEl);
+    assertTrue( $c1_19.find('p:eq(1)').nextAll().length == 4,$resultEl);
+    assertTrue( $c1_19.find('p:eq(1)').prevAll().length == 1,$resultEl);
+
+    var $c1_20 = $('#c1_20');
+    $resultEl = $c1_20.find('.result');
+
+    var $newEl = $('<p><a>JQuery</a></p>')
+        .find('a').attr('href','http://api.jquery.com/').end();
+
+    $c1_20.prepend($newEl);
+
+    // $('<p><a>JQuery</a></p>')
+    //     .find('a').attr('href','http://api.jquery.com/')
+    //     .end().prependTo($c1_20);
+
+    assertTrue($c1_20.find('p a').length === 1,$resultEl);
+
+    var $c1_21 = $('#c1_21');
+    $resultEl = $c1_21.find('.result');
+    // $c1_21.find('a').remove();
+    // assertTrue($c1_21.find('a').length === 0,$resultEl);
+
+    $c1_21.find('a').remove('.wantRemove');
+    assertTrue($c1_21.find('a.wantRemove').length === 0,$resultEl);
+    assertTrue($c1_21.find('a').length !== 0,$resultEl);
+
+    var $c1_22 = $('#c1_22');
+    $resultEl = $c1_22.find('.result');
+
+    $c1_22.find('button').on('click',function () {
+        //$(this).replaceWith('<p class="alert alert-primary" style="text-align: center">Replaced</p>')
+        $('<p class="alert alert-primary" style="text-align: center">'+$(this).text()+'</p>').replaceAll($(this));
+        assertTrue($c1_22.find('p').length === 1,$resultEl);
+    });
+
+
+    var $c1_23 = $('#c1_23');
+
+    $c1_23.find('li').on('click',function () { console.log('Clicked!'); })
+
+    $c1_23.find('button#copyOnlyElement').click('on',function () {
+        var copiedList = $c1_23.find('ul').clone().hide();
+        $c1_23.after(copiedList,$c1_23.find(':last'));
+        copiedList.animate({
+            height : 'toggle',
+            opacity : 'toggle'
+        })
+    });
+    
+    $c1_23.find('button#copyWithEvent').click('on',function () {
+        var copiedList = $c1_23.find('ul').clone(true).hide();
+        $c1_23.after(copiedList,$c1_23.find(':last'));
+        copiedList.animate({
+            height : 'toggle',
+            opacity : 'toggle'
+        })
+    });
+
+    var $c1_24 = $('#c1_24');
+    $resultEl = $c1_24.find('.result');
+
+    $c1_24.find('button').on('click',function () {
+        var hrefAttr = 'http://www.jquery.com';
+        var $anchorEl = $c1_24.find('a');
+        // $anchorEl.attr('href',hrefAttr).text('JQuery');
+        $anchorEl.attr({href : hrefAttr, title: 'jquery.com' }).text('JQuery');
+        assertTrue($c1_24.find('a').attr('href') === hrefAttr,$resultEl);
+        assertTrue($c1_24.find('a').attr('title') === 'jquery.com',$resultEl);
+    });
+
+    var $c1_25 = $('#c1_25');
+    $resultEl = $c1_25.find('.result');
+
+    $c1_25.find('button').on('click',function () {
+        $c1_25.find('p').html('<strong>Hello JQUERY!</strong> <em>Greeting!</em>');
+    })
 });
 
 /*
